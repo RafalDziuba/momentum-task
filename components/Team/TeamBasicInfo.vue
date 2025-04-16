6cfb348e004561f907d0ec4436a2f330f50a8ae6
Rafal Dziuba <dziuba.rafal98@gmail.com>
2025-04-16 19:00:40 +0200

    TeamBasicInfo refactor

--- components/Team/TeamBasicInfo.vue
+++ components/Team/TeamBasicInfo.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
-import { useTeamsStore } from '~/stores/teamsModule';
-import type { Team } from '~/types';
+  import { useTeamsStore } from '~/stores/teamsModule';
+  import type { Team } from '~/types';
+  import CalendarIcon from '../icons/CalendarIcon.vue';
+  import StadiumIcon from '../icons/StadiumIcon.vue';
+  import PersonIcon from '../icons/PersonIcon.vue';
+  import TeamInfoItem from '~/components/Team/TeamInfoItem.vue';
 
-defineProps<{
-  team: Team;
-}>();
 
-const teamsStore = useTeamsStore();
+  defineProps<{
+    team: Team;
+  }>();
+
+  const teamsStore = useTeamsStore();
 </script>
 
 <template>
@@ -23,67 +28,25 @@ const teamsStore = useTeamsStore();
     </div>
 
     <div class="space-y-3">
-      <div class="flex items-center space-x-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5 text-gray-500 dark:text-gray-400"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-          />
-        </svg>
-        <p class="dark:text-gray-300">
-          <strong>Founded:</strong> {{ team.founded }}
-        </p>
-      </div>
-      <div class="flex items-center space-x-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5 text-gray-500 dark:text-gray-400"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
-          />
-        </svg>
-        <p class="dark:text-gray-300">
-          <strong>Stadium:</strong> {{ team.stadium }}
+      <TeamInfoItem label="Founded" :value="team.founded">
+        <template #icon><CalendarIcon /></template>
+      </TeamInfoItem>
+
+      <TeamInfoItem label="Stadium" :value="team.stadium">
+        <template #icon><StadiumIcon /></template>
+        <template #action>
           <button
             @click="teamsStore.startEditingTeamDetails()"
             class="ml-2 text-blue-600 hover:text-blue-800 dark:text-blue-400 dark:hover:text-blue-300 text-sm"
           >
             Edit
           </button>
-        </p>
-      </div>
-      <div class="flex items-center space-x-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5 text-gray-500 dark:text-gray-400"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-          />
-        </svg>
-        <p class="dark:text-gray-300"><strong>Coach:</strong> {{ team.coach }}</p>
-      </div>
+        </template>
+      </TeamInfoItem>
+
+      <TeamInfoItem label="Coach" :value="team.coach">
+        <template #icon><PersonIcon /></template>
+      </TeamInfoItem>
     </div>
   </div>
 </template>
