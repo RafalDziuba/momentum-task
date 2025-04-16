bbf186e7de176225fb0d5beb8ececb201f725f00
Rafal Dziuba <dziuba.rafal98@gmail.com>
2025-04-16 19:57:44 +0200

    TeamMatchHistory refactor

--- components/Team/TeamMatchHistory.vue
+++ components/Team/TeamMatchHistory.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
-import { useTeamsStore } from '~/stores/teamsModule';
-import TeamRecentForm from './TeamRecentForm.vue';
-import TeamSeasonSummary from './TeamSeasonSummary.vue';
-import TeamMatchesTable from './TeamMatchesTable.vue';
-import type { Team } from '~/types';
+  import { useTeamsStore } from '~/stores/teamsModule';
+  import TeamRecentForm from './TeamRecentForm.vue';
+  import TeamSeasonSummary from './TeamSeasonSummary.vue';
+  import TeamMatchesTable from './TeamMatchesTable.vue';
+  import type { Team } from '~/types';
+  import TickIcon from '../icons/TickIcon.vue';
 
-defineProps<{
-  team: Team;
-}>();
+  defineProps<{
+    team: Team;
+  }>();
 
-const teamsStore = useTeamsStore();
+  const teamsStore = useTeamsStore();
 </script>
 
 <template>
@@ -21,20 +22,7 @@ const teamsStore = useTeamsStore();
       v-if="teamsStore.showEditSuccess"
       class="mb-4 p-3 bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200 rounded-md flex items-center"
     >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        class="h-5 w-5 mr-2"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke="currentColor"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M5 13l4 4L19 7"
-        />
-      </svg>
+      <TickIcon />
       Match result updated successfully! Standings have been recalculated.
     </div>
 
